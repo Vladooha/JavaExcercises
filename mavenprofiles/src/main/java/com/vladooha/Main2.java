72be70035c28579c47d9cacf091b2dcebc251534
Vladislav <mezhuev@lanit.ru>
2019-05-21 16:04:37 +0300

    Ex2 done

---
+++ mavenprofiles/src/main/java/com/vladooha/Main2.java
@@ -0,0 +1,12 @@
+package com.vladooha;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Main2 {
+    private static final Logger logger = LogManager.getLogger(Main1.class);
+
+    public static void main(String[] args) {
+        logger.error("Profile 2 started");
+    }
+}
